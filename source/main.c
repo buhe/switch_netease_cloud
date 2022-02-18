d0e45f5d175d3897e1101dd53fd2ba21c6378840
buhe <bugu1986@126.com>
2022-02-18 19:57:14 +0800

    u

--- source/main.c
+++ source/main.c
@@ -6,7 +6,7 @@
 int main(int argc, char* argv[]) {
     consoleInit(NULL);              // Initialize the console; redirect printf to the console
 
-    printf("Hello World!\n");       // Print Hello World to the console
+    printf("Hello bugu!\n");       // Print Hello World to the console
 
     while (appletMainLoop()) {      // while the application hasn't received an exit request...
         consoleUpdate(NULL);        // Update the screen
