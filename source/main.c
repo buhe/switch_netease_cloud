2d9697f63583da3eb35b3a7b485fb55adef79412
buhe <bugu1986@126.com>
2022-02-18 19:50:11 +0800

    init

---
+++ source/main.c
@@ -0,0 +1,17 @@
+#include <string.h>
+#include <stdio.h>
+
+#include <switch.h>
+
+int main(int argc, char* argv[]) {
+    consoleInit(NULL);              // Initialize the console; redirect printf to the console
+
+    printf("Hello World!\n");       // Print Hello World to the console
+
+    while (appletMainLoop()) {      // while the application hasn't received an exit request...
+        consoleUpdate(NULL);        // Update the screen
+    }
+
+    consoleExit(NULL);              // Clean up
+    return 0;
+}
